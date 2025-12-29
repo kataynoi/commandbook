a2f1dbe031b8b1f2aa9a6b16ef23aa60cf5be2be
kataynoi <thait-rex@hotmail.com>
2025-12-29 09:41:10 +0700

    EditUsers

--- src/app/Views/layout/partials/header.php
+++ src/app/Views/layout/partials/header.php
@@ -4,7 +4,7 @@ $userRoles = session()->get('roles') ?? [];
 ?>
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="<?= site_url('/dashboard'); ?>">หนังสือคำสั่ง  สสจ. Mahasarakham</a>
+    <a class="navbar-brand ps-3" href="<?= site_url('/dashboard'); ?>">HR หนังสือคำสั่ง  สสจ. Mahasarakham</a>
     <!-- Sidebar Toggle-->
     <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
